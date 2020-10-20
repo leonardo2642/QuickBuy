7fd64920fb53909935f54497aeda13a0a46aed51
Leonardo Dau <leonardo.dau@hotmail.com>
2020-10-20 11:04:01 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public int Descricao { get; set; }
+        public int Preco { get; set; }
+
+
+    }
+}
